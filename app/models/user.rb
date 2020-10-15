43281a7500bf8869304bd4ba956e71cb2a9afbbd
Timo Purzner <timo@purzner.dev>
2020-10-15 16:41:17 +0200

    CRUD for topic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :topics, foreign_key: :owner_id, class_name: 'Topic'
 
   def show_name
     return prename unless prename.nil?
+
     'Unbekannt'
   end
 end
